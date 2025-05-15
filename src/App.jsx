e69cd6da5374077e3aa28e230df6a59de7273045
upadhyaykartikeya <ukartik451@gmail.com>
2025-05-15 13:17:38 +0530

    contactUs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navigation from './components/Navigation/Navigation'
+import ContactHeader from './components/ContactHeader/ContactHeader'
 import './App.css'
 
 
 function App() {
   return <div>
     <Navigation />
+    <ContactHeader/>
   </div>
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import styles from "./Navigation.module.css";
 
 const Navigation = () => {
+
   return (
     <nav className={`${styles.navigation} container`}>
         <div className="logo">
