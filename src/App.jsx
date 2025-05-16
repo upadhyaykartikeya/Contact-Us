9d6dfb52ea3592b32e2d1be5ef55367e1d902501
upadhyaykartikeya <ukartik451@gmail.com>
2025-05-16 16:22:57 +0530

    contact form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navigation from './components/Navigation/Navigation'
 import ContactHeader from './components/ContactHeader/ContactHeader'
 import './App.css'
+import ContactForm from './components/ContactForm/ContactForm'
 
 
 function App() {
   return <div>
     <Navigation />
     <ContactHeader/>
+    <ContactForm />
   </div>
 }
 
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,11 @@
+import Button from "../Button/Button"
 import styles from "./Contact.module.css"
 
 const ContactForm = () => {
   return (
     <section className={styles.container}>
         <div className={styles.contact_form}></div>
+        <Button />
         <div className={styles.contact_image}></div>
     </section>
   )
