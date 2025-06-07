c8d779f7d960ef828ec1790a1a7d28c3581e966f
upadhyaykartikeya <ukartik451@gmail.com>
2025-06-07 11:21:17 +0530

    removed the unnecesary rendering

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,34 +5,42 @@ import { FaPhoneAlt } from "react-icons/fa";
 import {HiMail} from "react-icons/hi";
 
 const ContactForm = () => {
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("name", event.target[0].value);
+    console.log("email", event.target[1].value);
+    console.log("text", event.target[2].value);
+  }
+
   return (
     <section className={styles.container}>
         <div className={styles.contact_form}>
           <div className={styles.top_btn}>  
-        <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px"/>} />
-        <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="20px" />} />
+            <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px"/>} />
+            <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="20px" />} />
           </div>
-          <Button isOutline = {true} text="VIA EMAIL FORM" icon={<HiMail fontSize="20px" />} />
-
-          <form>
-          <div className={styles.form_control}>
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name"/>
-          </div>
-          <div className={styles.form_control}>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email"/>
-          </div>
-          <div className={styles.form_control}>
-            <label htmlFor="text">Text</label>
-            <textarea name="text" rows="10"/>
-          </div>
-          <div style={{
-            display: "flex",
-            justifyContent: "end"
-          }}>
-          <Button text="SUBMIT BUTTON"/>
-          </div>
+            <Button isOutline = {true} text="VIA EMAIL FORM" icon={<HiMail fontSize="20px" />} />
+          <form onSubmit={onSubmit} >
+            <div className={styles.form_control}>
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name"/>
+            </div>
+            <div className={styles.form_control}>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email"/>
+            </div>
+            <div className={styles.form_control}>
+              <label htmlFor="text">Text</label>
+              <textarea name="text" rows="10"/>
+            </div>
+            <div style={{
+              display: "flex",
+              justifyContent: "end"
+              }}>
+              <Button text="SUBMIT BUTTON"/>
+            </div>
           </form>
 
 
